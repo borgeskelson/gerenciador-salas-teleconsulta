fd0908093f8f7c1249bbd036acdc104f1fafb62b
Kelson Borges <kelson.borges@inema.ba.gov.br>
2025-11-02 23:16:47 -0300

    Adicionando página com Primefaces e configurações para web view

--- src/main/java/br/gov/ba/saude/teleconsulta/TeleconsultaApplication.java
+++ src/main/java/br/gov/ba/saude/teleconsulta/TeleconsultaApplication.java
@@ -2,12 +2,21 @@ package br.gov.ba.saude.teleconsulta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeleconsultaApplication {
+@ServletComponentScan
+public class TeleconsultaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeleconsultaApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TeleconsultaApplication.class);
+    }
 
 }
---
+++ src/main/java/br/gov/ba/saude/teleconsulta/config/WebConfig.java
@@ -0,0 +1,14 @@
+package br.gov.ba.saude.teleconsulta.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.xhtml");
+	}
+
+}
