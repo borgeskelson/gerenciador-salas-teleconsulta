f70d077be98e78323c0b173e5e75892c30a27381
Kelson Borges <kelson.borges@inema.ba.gov.br>
2025-11-03 01:23:02 -0300

    Inclui campo para definição e edição de senha no cadastro de Usuário

--- src/main/java/br/gov/ba/saude/teleconsulta/controller/UsuarioController.java
+++ src/main/java/br/gov/ba/saude/teleconsulta/controller/UsuarioController.java
@@ -23,6 +23,7 @@ public class UsuarioController implements Serializable {
 	
 	private List<Usuario> usuarios;
 	private Usuario selecionado;
+	private String novaSenha; // Campo auxiliar para a senha
 	private final List<String> perfis = List.of("ADMINISTRADOR", "RESTRITO");
 	private boolean edicao;
 	
@@ -50,6 +51,7 @@ public class UsuarioController implements Serializable {
 			this.selecionado = new Usuario();
 			this.edicao = false;
 		}
+		this.novaSenha = null; // Limpa a senha ao preparar novo
 	}
 	
 	public void editar(Usuario selecionado) {
@@ -62,6 +64,11 @@ public class UsuarioController implements Serializable {
 	 */
 	public void salvar() {
 		try {
+			// Se for um novo usuário ou a senha foi alterada, criptografa a nova senha
+			if (selecionado.getId() == null || novaSenha != null && !novaSenha.isEmpty()) {
+				selecionado.setSenha(novaSenha);
+			}
+			
 			// Garante que o perfil básico seja setado (RESTRITO)
 			if (selecionado.getPerfil() == null || selecionado.getPerfil().isEmpty()) {
 				selecionado.setPerfil("RESTRITO");
@@ -134,6 +141,14 @@ public class UsuarioController implements Serializable {
 	
 	public void setSelecionado(Usuario selecionado) {
 		this.selecionado = selecionado;
+	}
+	
+	public String getNovaSenha() {
+		return novaSenha;
+	}
+
+	public void setNovaSenha(String novaSenha) {
+		this.novaSenha = novaSenha;
 	}
 	
 	public List<String> getPerfis() {
