0afb431ade4942ae8eee1384ad89cbc9457391b4
Kelson Borges <kelson.borges@inema.ba.gov.br>
2025-11-02 22:42:53 -0300

    Commit inicial

---
+++ src/main/java/br/gov/ba/saude/teleconsulta/TeleconsultaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ba.saude.teleconsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleconsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleconsultaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ba/saude/teleconsulta/TeleconsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ba.saude.teleconsulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleconsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
